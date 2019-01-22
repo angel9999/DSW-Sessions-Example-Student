9825583b5fd901ca646955a1feab73cd9e32cd74
angel9999 <31519796+angel9999@users.noreply.github.com>
2019-01-22 11:03:01 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -19,7 +19,7 @@ def renderMain():
 @app.route('/startOver')
 def startOver():
     #TODO: delete everything from the session
-    
+    session.clear()
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
 @app.route('/page1')
@@ -29,13 +29,14 @@ def renderPage1():
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
     #TODO: set the first and last name in the session
-    
+    session["firstName"] = request.from["firstName"] # adds the first name to the cookie
+    session["lastName"] = request.from["lastName"] # adds the first name to the cookie
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
     #TODO: set the favorite color in the session
-    
+    seesion["favoriteColor"] = request.from["favoriteColor"]
     return render_template('page3.html')
     
 if __name__=="__main__":
